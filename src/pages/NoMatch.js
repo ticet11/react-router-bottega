9099d07fbd772c4863da7e37a9c35210b56c102e
Brian Kozub <ticet11@gmail.com>
2020-05-19 11:07:39 -0600

    NoMatch page working.

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function noMatch() {
+export default function NoMatch() {
     return (
         <div>
             <h2>Wow, it's like that's not even a page, to be honest.</h2>
